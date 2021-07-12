fae82e62f5292f0097188afed019d3056160c48b
Mathias Bramstorp <m.bramstorp@stofanet.dk>
2021-07-12 21:03:18 +0200

    small update for demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Layout } from "./Layout/Layout";
 import { Home } from "./Home/Home";
-import { Profile } from "./Profile/Profile";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Layout>
         <Switch>
           <Route path="/" exact={true} component={Home} />
-          <Route path="/profile" exact={true} component={Profile} />
         </Switch>
       </Layout>
     </BrowserRouter>
--- src/Home/modules/HomeDemo.tsx
+++ src/Home/modules/HomeDemo.tsx
@@ -2,9 +2,12 @@ import React, { FunctionComponent } from "react";
 
 export const HomeDemo: FunctionComponent = () => (
   <div>
-    <h1 className="text-center pt-4">Home</h1>
-    <a className="text-center pt-4" href="/profile">
-      Profile
-    </a>
+    <h1 className="text-center pt-4">Car Wash Booking System</h1>
+    <p className="text-center pt-2">Welcome choose from of cleaning</p>
+    <div className="row">
+      <div>Gold</div>
+      <div>Platinum</div>
+      <div>Silver</div>
+    </div>
   </div>
 );
